8fcd85bf16647e766981a964f902da7b0e84d95c
Emily Baker-King <emilybaker-king@interapts-macbook-pro-102.local>
2018-09-12 10:09:01 -0500

    Initial Commit

---
+++ Task Manager/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Task Manager
+//
+//  Created by Emily Baker-King on 9/12/18.
+//  Copyright © 2018 Emily Baker-King. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
